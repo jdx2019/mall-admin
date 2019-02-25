0c749ad1755c6093bf8eb3c5d4f10ddf242f744d
Your Name <you@example.com>
2019-02-25 21:31:17 +0800

    111

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -66,6 +66,16 @@ module.exports = {
           name: utils.assetsPath('media/[name].[hash:7].[ext]')
         }
       },
+      
+      {
+        test: /\.scss$/,
+        use: [
+            // {
+            //     loader: 'sass-loader' // compiles Sass to CSS
+            // }
+        ]
+      },
+
       {
         test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
         loader: 'url-loader',
--- config/dev.env.js
+++ config/dev.env.js
@@ -4,5 +4,7 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  BASE_API: '"http://localhost:8080"'
+  // BASE_API: '"http://localhost:8080"'
+  BASE_API: '"https://39.108.193.144:8080"'
+  // BASE_API: '"http://39.98.190.128:8080"'
 })
